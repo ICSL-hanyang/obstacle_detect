d5c63438ccfdb27a791f11558f7211ec1cd20433
kissw <kiminstein@naver.com>
2019-07-07 18:40:58 +0900

    gg

--- include/obstacle_detect.h
+++ include/obstacle_detect.h
@@ -1,4 +1,51 @@
 #ifndef OBSTACLE_DETECT
 #define OBSTACLE_DETECT
 #include <ros/ros.h>
+#include <string.h>
+#include "obstacle_detect/Orientation.h"
+
+class Detection
+{
+ private :
+    double lidar_left;
+    double lidar_right;
+    double lidar_front;
+    double lidar_back;
+    obstacle_detect::Orientation orient;
+ public :
+
+    bool left = true, right = true, front = true, back = true;
+    Detection()
+    {
+        lidar_left   = 0;
+        lidar_right  = 0;
+        lidar_front  = 0;
+        lidar_back   = 0;
+        orient.curr = "hovering";
+        orient.prev = "hovering";
+    };
+    ~Detection(){};
+    void setLeft( double n ) { lidar_left = n; }
+    void setRight( double n ) { lidar_right = n; }
+    void setFront( double n ) { lidar_front = n; }
+    void setBack( double n ) { lidar_back = n; }
+    double getLeft() { return lidar_left; }
+    double getRight() { return lidar_right; }
+    double getFront() { return lidar_front; }
+    double getBack() { return lidar_back; }
+    std::string getState() { return orient.curr; }
+    void setState( std::string s )
+    {
+        orient.prev = getState();
+        orient.curr = s;
+    }
+    void goFront() { setState("front"); }
+    void goBack() { setState("back"); }
+    void goLeft() { setState("left"); }
+    void goRight() { setState("right"); }
+    void hovering() { setState("hovering"); }
+    obstacle_detect::Orientation getOrient() { return orient; }
+    obstacle_detect::Orientation orientationDetect();
+};
+
 #endif// !1
--- src/obstacle_detect.cpp
+++ src/obstacle_detect.cpp
@@ -2,26 +2,35 @@
 #include <cmath>
 #include <limits>
 #include <ros/ros.h>
+#include <string.h>
 #include <sensor_msgs/LaserScan.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <obstacle_detect.h>
 #include "obstacle_detect/Pair.h"
 #include "obstacle_detect/VectorPair.h"
+#include "obstacle_detect/Orientation.h"
 
 obstacle_detect::VectorPair vector_pair;
 obstacle_detect::Pair pair;
+obstacle_detect::Orientation o;
 double rp_arr[360]={0.0,};
 uint8_t degree = 5; // 몇도 마다 자를래
 int num = 360/degree; // 원하는 각도로 잘랐을때 나오는 구역의 수
 double detecting_range = 2.0;  //detecting_range 보다 작은 값만 받을거다!
-
+Detection detect;
+double x,y,px,py;
+int sec, psec;
+bool sec_flag = true;
 void rplidarCB(const sensor_msgs::LaserScan::ConstPtr &msg)
 {
     double msg_arr[num]={0.0,}; //배열 초기화
     uint8_t n = 0;              //카운트용
     uint8_t inf_n = 0;          //무한대카운트용
-    // printf("0 : %lf\n",msg->ranges[0]);
-    // printf("90 : %lf\n",msg->ranges[90]);
-    // printf("180 : %lf\n",msg->ranges[180]);
-    // printf("270 : %lf\n",msg->ranges[270]);
+    detect.setLeft(msg->ranges[0]);
+    detect.setRight(msg->ranges[180]);
+    detect.setFront(msg->ranges[270]);
+    detect.setBack(msg->ranges[90]);
+
     for(uint16_t i = 0; i<360; i++) //0부터 359도에 대한 센서값을 받아오기위해
     {                               
         
@@ -47,26 +56,110 @@ void rplidarCB(const sensor_msgs::LaserScan::ConstPtr &msg)
     }
     // printf("------------------------------------------------------\n");
 }
-
-int orientationDetect()
+void localPositionCB(const geometry_msgs::PoseStamped::ConstPtr &msg)
 {
-    //
-    return 0;
+    if(sec_flag)
+    {
+        sec = msg->header.stamp.sec;
+        x = msg->pose.position.x;
+        y = msg->pose.position.y;
+    }
+    psec = msg->header.stamp.sec;
+    px = msg->pose.position.x;
+    py = msg->pose.position.y;
+}
+
+obstacle_detect::Orientation Detection::orientationDetect()
+{
+    //left right hovering front back
+    // ROS_INFO_STREAM(detect.getLeft());
+    // ROS_INFO_STREAM(detect.getRight());
+    // ROS_INFO_STREAM(detect.getFront());
+    // ROS_INFO_STREAM(detect.getBack());
+    if( detect.getLeft() <= detecting_range ) { this->left = false; hovering(); }
+    if( detect.getRight() <= detecting_range ) { this->right = false; hovering(); }
+    if( detect.getFront() <= detecting_range ) { this->front = false; hovering(); }
+    // if( detect.getBack() <= detecting_range ) { this->back = false; hovering(); }
+    if( detect.getState() == "hovering" )
+    { 
+        if(this->front && detect.getFront() >= detecting_range )
+        {
+            this->right = true;
+            // this->back = true;
+            this->left = true;
+            this->front = true;
+            sec_flag = true;
+            if(this->front)
+                detect.goFront();
+        }
+        //         if(detect.getLeft() >= detecting_range )
+        if(!this->front && detect.getLeft() >= detecting_range )
+        {
+            // this->left = true;
+            if(this->left)
+                detect.goLeft();
+            if( detect.getFront() >= detecting_range)
+                this->front = true;
+        }
+
+        // if(!this->left && detect.getRight() >= detecting_range )
+        if(!this->front && !this->left && detect.getRight() >= detecting_range )
+        {
+            if(this->right)
+                detect.goRight();
+            if( detect.getFront() >= detecting_range)
+                this->front = true;
+        }
+        // if(!this->front && !this->left && !this->right && detect.getBack() >= detecting_range )
+        // {
+        //     sec_flag = false;
+        //     double distance = 10;
+        //     if( abs(sec-psec)>=10 )
+        //     {
+        //         distance = sqrt(pow(x-px, 2.0)+pow(y-py, 2.0));
+        //     }
+        //     if(this->back)
+        //         detect.goBack();
+        //     if(distance <= 2)
+        //     {
+        //         this->right = true;
+        //         this->back = true;
+        //         this->left = true;
+        //         this->front = true;
+        //         sec_flag = true;
+        //     }
+        // }
+        // if( !this->left && !this->right && !this->front && !this->back )
+        if( !this->left && !this->right )
+        {
+            detect.hovering();
+            // printf("------------------------------------------------------------\n");
+            this->right = true;
+            // this->back = true;
+            this->left = true;
+            this->front = true;
+            sec_flag = true;
+        }
+    }
+    return detect.getOrient();
 }
 
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "obstacle_detect_node");
 	ros::NodeHandle nh("");
-	ros::Rate rate(5);
+	ros::Rate rate(10);
     ros::Subscriber rplidar_sub = nh.subscribe("camila1/laser/scan", 10, &rplidarCB);
+    ros::Subscriber local_pose = nh.subscribe("mavros/local_position/pose", 10, &localPositionCB);
     ros::Publisher rplidar_pub = nh.advertise<obstacle_detect::VectorPair>("/vector_pair",10);
+    ros::Publisher orient_pub = nh.advertise<obstacle_detect::Orientation>("/orientation",10);
+    obstacle_detect::Orientation a;
     while (ros::ok())
 	{
         nh.getParamCached("detecting_range", detecting_range);
         for(uint16_t i=0; i<num; i++)
         {
-
+            a = detect.orientationDetect();
             // printf("angle : %10lf \t distance : %10lf\n", pair.angle, pair.distance);
             if(rp_arr[i] == std::numeric_limits<double>::infinity())
                 continue;
@@ -80,6 +173,7 @@ int main(int argc, char **argv)
         // else vector_pair.isEmpty = true;
 
         rplidar_pub.publish(vector_pair);
+        orient_pub.publish(a);
         vector_pair.data.clear();
 		ros::spinOnce();
 		rate.sleep();
